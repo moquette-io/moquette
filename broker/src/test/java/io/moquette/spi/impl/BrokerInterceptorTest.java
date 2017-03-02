2960fb874004f5f24549452fa65846bfbe1bde86
Andreas Mager <ama@qsc.de>
2017-03-02 16:50:00 +0100

    broker/InterceptUnsubscribeMessage: Topic added

--- broker/src/test/java/io/moquette/spi/impl/BrokerInterceptorTest.java
+++ broker/src/test/java/io/moquette/spi/impl/BrokerInterceptorTest.java
@@ -148,7 +148,7 @@ public class BrokerInterceptorTest {
 
     @Test
     public void testNotifyTopicUnsubscribed() throws Exception {
-        interceptor.notifyTopicUnsubscribed("o2", "cli1234", "cli1234");
+        interceptor.notifyTopicUnsubscribed(new Topic("o2"), "cli1234", "cli1234");
         interval();
         assertEquals(80, n.get());
     }
