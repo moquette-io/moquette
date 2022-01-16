09acfbd1663c85f6f37245996d2f673e5b54f299
andsel <selva.andre@gmail.com>
2022-01-16 19:20:00 +0100

    Switched from Paho sync to async API to make the test faster and more concurrent

--- broker/src/test/java/io/moquette/integration/IntegrationUtils.java
+++ broker/src/test/java/io/moquette/integration/IntegrationUtils.java
@@ -16,14 +16,16 @@
 
 package io.moquette.integration;
 
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Properties;
+
 import static io.moquette.BrokerConstants.DEFAULT_MOQUETTE_STORE_H2_DB_FILENAME;
 import static io.moquette.BrokerConstants.PERSISTENT_STORE_PROPERTY_NAME;
 import static io.moquette.BrokerConstants.PORT_PROPERTY_NAME;
@@ -63,6 +65,18 @@ public final class IntegrationUtils {
     }
 
     static void disconnectClient(IMqttClient client) throws MqttException {
+        if (client != null && client.isConnected()) {
+            client.disconnect();
+        }
+    }
+
+    static void disconnectClient(IMqttAsyncClient client, IMqttActionListener callback) throws MqttException {
+        if (client != null && client.isConnected()) {
+            client.disconnect(null, callback);
+        }
+    }
+
+    static void disconnectClient(IMqttAsyncClient client) throws MqttException {
         if (client != null && client.isConnected()) {
             client.disconnect();
         }
--- broker/src/test/java/io/moquette/integration/PublishToManySubscribersUseCaseTest.java
+++ broker/src/test/java/io/moquette/integration/PublishToManySubscribersUseCaseTest.java
@@ -6,25 +6,27 @@ import io.moquette.broker.config.IConfig;
 import io.moquette.broker.config.MemoryConfig;
 import org.awaitility.Awaitility;
 import org.awaitility.Durations;
-import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
 
@@ -34,6 +36,8 @@ import static org.hamcrest.Matchers.equalTo;
 
 // inspired by ServerIntegrationPahoTest
 public class PublishToManySubscribersUseCaseTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PublishToManySubscribersUseCaseTest.class);
 
     private static final int COMMAND_QUEUE_SIZE = 32;
 
@@ -45,13 +49,12 @@ public class PublishToManySubscribersUseCaseTest {
     @TempDir
     Path tempFolder;
     private String dbPath;
-    private MqttClient publisher;
-    private List<IMqttClient> subscribers;
+    private MqttAsyncClient publisher;
+    private List<IMqttAsyncClient> subscribers;
 
     protected void startServer(String dbPath) throws IOException {
         broker = new Server();
         final Properties configProps = IntegrationUtils.prepareTestProperties(dbPath);
-//        configProps.put(BrokerConstants.IMMEDIATE_BUFFER_FLUSH_PROPERTY_NAME, "true");
         configProps.put(BrokerConstants.SESSION_QUEUE_SIZE, Integer.toString(COMMAND_QUEUE_SIZE));
         brokerConfig = new MemoryConfig(configProps);
         broker.startServer(brokerConfig);
@@ -68,58 +71,81 @@ public class PublishToManySubscribersUseCaseTest {
         startServer(dbPath);
 
         publisher = createClient("publisher");
-        publisher.connect();
+        publisher.connect().waitForCompletion(1_000);
 
         subscribers = createSubscribers(NUM_SUBSCRIBERS);
-        for (IMqttClient client : subscribers) {
-            client.connect();
+
+        CountDownLatch latch = new CountDownLatch(NUM_SUBSCRIBERS);
+
+        final IMqttActionListener callback = createMqttCallback(latch);
+        for (IMqttAsyncClient client : subscribers) {
+            client.connect(null, callback);
         }
+
+        latch.await();
+        LOG.debug("After all subscriptions completed");
     }
 
-    private List<IMqttClient> createSubscribers(int numSubscribers) throws MqttException, IOException {
-        List<IMqttClient> clients = new ArrayList<>(numSubscribers);
+    private IMqttActionListener createMqttCallback(CountDownLatch latch) {
+        return new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                latch.countDown();
+            }
+        };
+    }
+
+    private List<IMqttAsyncClient> createSubscribers(int numSubscribers) throws MqttException, IOException {
+        List<IMqttAsyncClient> clients = new ArrayList<>(numSubscribers);
         for (int i = 0; i < numSubscribers; i++) {
             clients.add(createClient("subscriber_" + i));
         }
         return clients;
     }
 
-    private MqttClient createClient(String clientName) throws IOException, MqttException {
+    private MqttAsyncClient createClient(String clientName) throws IOException, MqttException {
         final String dataPath = IntegrationUtils.newFolder(tempFolder, clientName).getAbsolutePath();
         MqttClientPersistence clientDataStore = new MqttDefaultFilePersistence(dataPath);
-        return new MqttClient("tcp://localhost:1883", clientName, clientDataStore);
+        return new MqttAsyncClient("tcp://localhost:1883", clientName, clientDataStore);
     }
 
     @AfterEach
     public void tearDown() throws Exception {
         IntegrationUtils.disconnectClient(this.publisher);
-        disconnectClients(this.subscribers);
+        disconnectAsyncClients(this.subscribers);
 
         this.broker.stopServer();
     }
 
-    private void disconnectClients(List<IMqttClient> clients) throws MqttException {
-        for (IMqttClient client : clients) {
-            IntegrationUtils.disconnectClient(client);
+    private void disconnectAsyncClients(List<IMqttAsyncClient> clients) throws MqttException, InterruptedException {
+        CountDownLatch latch = new CountDownLatch(clients.size());
+        final IMqttActionListener completionCallback = createMqttCallback(latch);
+        for (IMqttAsyncClient client : clients) {
+            IntegrationUtils.disconnectClient(client, completionCallback);
         }
+        latch.await();
     }
 
     @Test
-    void onePublishTriggerManySubscriptionsNotifications() throws MqttException {
+    void onePublishTriggerManySubscriptionsNotifications() throws MqttException, InterruptedException {
         final LongAdder receivedPublish = new LongAdder();
 
         //subscribe all
-        for (IMqttClient subscriber : this.subscribers) {
+        for (IMqttAsyncClient subscriber : this.subscribers) {
             subscriber.subscribe("/temperature", 1, (String topic1, org.eclipse.paho.client.mqttv3.MqttMessage message) -> {
-                receivedPublish.increment();
-            });
+                    receivedPublish.increment();
+                }).waitForCompletion();
         }
 
         this.publisher.publish("/temperature", "15°C".getBytes(UTF_8), 0, false);
 
         Awaitility.await("Waiting for resend.")
             .atMost(FLIGHT_BEFORE_RESEND_MS * 3, TimeUnit.MILLISECONDS)
-//            .pollDelay(FLIGHT_BEFORE_RESEND_MS * 2, TimeUnit.MILLISECONDS)
             .pollInterval(100, TimeUnit.MILLISECONDS)
             .untilAdder(receivedPublish, equalTo((long) NUM_SUBSCRIBERS));
     }
