33d70534b6dd2ac7f2bf506921808af23cc35bee
Stepan Baranov <s.baranov@iqmessenger.com>
2022-02-01 16:18:45 +0200

    remove System.exit(1)

--- broker/src/main/java/io/moquette/broker/NewNettyAcceptor.java
+++ broker/src/main/java/io/moquette/broker/NewNettyAcceptor.java
@@ -223,7 +223,7 @@ class NewNettyAcceptor {
         } catch (Exception ex) {
             if (ex instanceof BindException) {
                LOG.error("Cannot bind to port: " + port, ex);
-               System.exit(1);
+                throw new RuntimeException(ex);
             } else {
                 LOG.error("An interruptedException was caught while initializing integration. Protocol={}", protocol, ex);
                 throw new RuntimeException(ex);
