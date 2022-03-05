43214c37d8b3f4d92518bba3087e388c481f3499
Hylke van der Schaaf <hylke.vds@gmail.com>
2022-03-05 09:13:13 +0100

    Fix #659: use buffer.toString(Charset) to directly get a String (#660)
    When the goal is to turn the contents of a buffer into a String it is
    more efficient and simpler to directly use the toString(Charset) method
    of byteBuf instead of fetching the byte data and manually creating a
    string.

--- broker/src/main/java/io/moquette/broker/DebugUtils.java
+++ broker/src/main/java/io/moquette/broker/DebugUtils.java
@@ -23,17 +23,7 @@ import java.nio.charset.StandardCharsets;
 public final class DebugUtils {
 
     public static String payload2Str(ByteBuf content) {
-        final ByteBuf copy = content.copy();
-        final byte[] bytesContent;
-        if (copy.isDirect()) {
-            final int size = copy.readableBytes();
-            bytesContent = new byte[size];
-            copy.readBytes(bytesContent);
-            copy.release();
-        } else {
-            bytesContent = copy.array();
-        }
-        return new String(bytesContent, StandardCharsets.UTF_8);
+        return content.copy().toString(StandardCharsets.UTF_8);
     }
 
     private DebugUtils() {
