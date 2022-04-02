17dffffc92665b1b840dcc6bcd7a2eaa6db85c62
Hylke van der Schaaf <hylke.vds@gmail.com>
2022-04-02 10:59:13 +0200

    Fixed #667: NPE in InterceptConnectMessage.getPassword (#670)
    InterceptConnectMessage.getPassword used msg.payload().password().getBytes
    but msg.payload() could return null. Additionally, msg.payload() is
    depricated. Instead, InterceptConnectMessage.getPassword now directly
    returns msg.payload().passwordInBytes() and thus never throws a NPE.
    
    * Changed use of MqttConnectPayload.willMessage
    
    MqttConnectPayload.willMessage() is deprecated, and since we want
    a byte[] anyway, we now use willMessageInBytes().

--- broker/src/main/java/io/moquette/interception/messages/InterceptConnectMessage.java
+++ broker/src/main/java/io/moquette/interception/messages/InterceptConnectMessage.java
@@ -18,8 +18,6 @@ package io.moquette.interception.messages;
 
 import io.netty.handler.codec.mqtt.MqttConnectMessage;
 
-import java.nio.charset.StandardCharsets;
-
 public class InterceptConnectMessage extends InterceptAbstractMessage {
 
     private final MqttConnectMessage msg;
@@ -74,7 +72,7 @@ public class InterceptConnectMessage extends InterceptAbstractMessage {
     }
 
     public byte[] getPassword() {
-        return msg.payload().password().getBytes(StandardCharsets.UTF_8);
+        return msg.payload().passwordInBytes();
     }
 
     public String getWillTopic() {
@@ -82,6 +80,6 @@ public class InterceptConnectMessage extends InterceptAbstractMessage {
     }
 
     public byte[] getWillMessage() {
-        return msg.payload().willMessage().getBytes(StandardCharsets.UTF_8);
+        return msg.payload().willMessageInBytes();
     }
 }
