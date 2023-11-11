13059a37987e5108a51266d5247f0a0f9db2f919
andsel <selva.andre@gmail.com>
2023-11-11 16:27:13 +0100

    Removed unused method

--- broker/src/main/java/io/moquette/broker/subscriptions/Token.java
+++ broker/src/main/java/io/moquette/broker/subscriptions/Token.java
@@ -34,18 +34,6 @@ public class Token implements Comparable<Token> {
         return name;
     }
 
-    protected boolean match(Token t) {
-        if (MULTI.equals(t) || SINGLE.equals(t)) {
-            return false;
-        }
-
-        if (MULTI.equals(this) || SINGLE.equals(this)) {
-            return true;
-        }
-
-        return equals(t);
-    }
-
     @Override
     public int hashCode() {
         int hash = 7;
