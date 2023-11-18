bbba65ec16551d989302771a1967d0c5a5cf578e
andsel <selva.andre@gmail.com>
2023-11-18 18:24:28 +0100

    Added test to validate the normal flow of shared-subscription and publish interaction

--- broker/src/test/java/io/moquette/integration/mqtt5/SharedSubscriptionTest.java
+++ broker/src/test/java/io/moquette/integration/mqtt5/SharedSubscriptionTest.java
@@ -1,8 +1,10 @@
 package io.moquette.integration.mqtt5;
 
 import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAckReasonCode;
+import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
 import io.netty.handler.codec.mqtt.MqttConnAckMessage;
 import io.netty.handler.codec.mqtt.MqttMessage;
 import io.netty.handler.codec.mqtt.MqttQoS;
@@ -10,6 +12,10 @@ import io.netty.handler.codec.mqtt.MqttReasonCodeAndPropertiesVariableHeader;
 import io.netty.handler.codec.mqtt.MqttReasonCodes;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static io.moquette.integration.mqtt5.ConnectTest.assertConnectionAccepted;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,6 +39,22 @@ public class SharedSubscriptionTest extends AbstractServerIntegrationTest {
         assertEquals(MqttReasonCodes.Disconnect.MALFORMED_PACKET.byteValue(), disconnectHeader.reasonCode());
     }
 
+    @Test
+    public void givenASharedSubscriptionClientReceivesANotification() throws InterruptedException {
+        final Mqtt5BlockingClient subscriberClient = createSubscriberClient();
+        subscriberClient.subscribeWith()
+            .topicFilter("$share/collectors/metric/temperature/#")
+            .send();
+
+        Mqtt5BlockingClient publisherClient = createPublisherClient();
+        publisherClient.publishWith()
+                .topic("metric/temperature/living")
+                .payload("18".getBytes(StandardCharsets.UTF_8))
+                .send();
+
+        verifyPublishedMessage(subscriberClient, 10, "18", "Shared message must be received");
+    }
+
     @NotNull
     private Mqtt5BlockingClient createSubscriberClient() {
         final Mqtt5BlockingClient client = MqttClient.builder()
@@ -43,5 +65,27 @@ public class SharedSubscriptionTest extends AbstractServerIntegrationTest {
             .buildBlocking();
         assertEquals(Mqtt5ConnAckReasonCode.SUCCESS, client.connect().getReasonCode(), "Subscriber connected");
         return client;
+    }
+
+    @NotNull
+    private Mqtt5BlockingClient createPublisherClient() {
+        final Mqtt5BlockingClient client = MqttClient.builder()
+            .useMqttVersion5()
+            .identifier("publisher")
+            .serverHost("localhost")
+            .serverPort(1883)
+            .buildBlocking();
+        assertEquals(Mqtt5ConnAckReasonCode.SUCCESS, client.connect().getReasonCode(), "Publisher connected");
+        return client;
+    }
+
+    private static void verifyPublishedMessage(Mqtt5BlockingClient subscriber, int timeout, String expectedPayload, String message) throws InterruptedException {
+        try (Mqtt5BlockingClient.Mqtt5Publishes publishes = subscriber.publishes(MqttGlobalPublishFilter.ALL)) {
+            Optional<Mqtt5Publish> publishMessage = publishes.receive(timeout, TimeUnit.SECONDS);
+            final String payload = publishMessage.map(Mqtt5Publish::getPayloadAsBytes)
+                .map(b -> new String(b, StandardCharsets.UTF_8))
+                .orElse("Failed to load payload");
+            assertEquals(expectedPayload, payload, message);
+        }
     }
 }
