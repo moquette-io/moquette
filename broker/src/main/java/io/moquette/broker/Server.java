749f1075ad53ca32ed2ccf5f1c19d5151a197c7e
andsel <selva.andre@gmail.com>
2024-12-19 12:06:49 +0100

    Moved 0.18 to final stage

--- broker/src/main/java/io/moquette/broker/Server.java
+++ broker/src/main/java/io/moquette/broker/Server.java
@@ -73,7 +73,7 @@ import static io.moquette.logging.LoggingUtils.getInterceptorIds;
 public class Server {
 
     private static final Logger LOG = LoggerFactory.getLogger(io.moquette.broker.Server.class);
-    public static final String MOQUETTE_VERSION = "0.18-SNAPSHOT";
+    public static final String MOQUETTE_VERSION = "0.18";
 
     private ScheduledExecutorService scheduler;
     private NewNettyAcceptor acceptor;
