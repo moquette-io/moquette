95dc3d18298c753bb3f960b45d37de397aa5c0bc
Andrea Selva <selva.andre@gmail.com>
2024-12-27 12:58:48 +0100

    Bump 0.19-SNAPSHOT dev cycle (#887)

--- distribution/src/main/scripts/moquette.sh
+++ distribution/src/main/scripts/moquette.sh
@@ -13,7 +13,7 @@ echo "  \_|  |_/\___/ \__, |\__,_|\___|\__|\__\___| \_|  |_/\_/\_\ \_/   \_/   "
 echo "                   | |                                                   "
 echo "                   |_|                                                   "
 echo "                                                                         "
-echo "                                               version: 0.18-SNAPSHOT    "
+echo "                                               version: 0.19-SNAPSHOT    "
 
 
 unset CDPATH
