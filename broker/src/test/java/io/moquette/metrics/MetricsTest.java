0f04461a0968febe10475226f51fe74d0249984a
Hylke van der Schaaf <hylke.vds@gmail.com>
2026-05-20 15:17:44 +0200

    Fixed metrics session mis-counting (#940)
    Fix a session-accounting-mistake in the session-reopen code, causing the Metrics to miscount sessions. I was seeing negative session counts

--- broker/src/test/java/io/moquette/metrics/MetricsTest.java
+++ broker/src/test/java/io/moquette/metrics/MetricsTest.java
@@ -25,15 +25,17 @@ import io.netty.handler.codec.mqtt.MqttQoS;
 import java.io.IOException;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.Properties;
-import java.util.logging.Level;
 import org.awaitility.Awaitility;
 import org.awaitility.Durations;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -107,15 +109,17 @@ public class MetricsTest {
         server.stopServer();
     }
 
+    private MetricsProviderMock validateMetricsProvider() {
+        // Setup, check if the mock metrics provider is used and is clean.
+        MetricsProvider metricsProvider = server.getMetricsProvider();
+        assertTrue(metricsProvider instanceof MetricsProviderMock, "MetricsProvider should be of type MetricsProviderMock, found " + metricsProvider.getClass());
+        MetricsProviderMock mp = (MetricsProviderMock) metricsProvider;
+        return mp;
+    }
+
     @Test
     public void testMetrics() throws MqttException {
-        MetricsProvider metricsProvider = server.getMetricsProvider();
-        MetricsProviderMock mp = null;
-        if (metricsProvider instanceof MetricsProviderMock) {
-            mp = (MetricsProviderMock) metricsProvider;
-        } else {
-            Assertions.fail("MetricsProvider should be of type MetricsProviderMock, found " + metricsProvider.getClass());
-        }
+        MetricsProviderMock mp = validateMetricsProvider();
 
         assertEquals(0, mp.getSessionCount());
         assertEquals(0, mp.getPublishCount());
@@ -144,12 +148,10 @@ public class MetricsTest {
         clientListener.disconnect();
         clientPublisher.disconnect();
 
-        try {
-            // Sleep shortly to give the metrics time to update.
-            Thread.sleep(100);
-        } catch (InterruptedException ex) {
-            // It's fine
-        }
+        Awaitility.await("Wait for the metrics to update.")
+            .pollInterval(Duration.ofMillis(10))
+            .atMost(Duration.ofMillis(100))
+            .until(() -> mp.getSessionCount(), Is.is(0));
 
         assertEquals(0, mp.getSessionCount());
         assertEquals(1, mp.getPublishCount());
@@ -161,6 +163,63 @@ public class MetricsTest {
         assertTrue(mp.getSessionQueueFillMax() > 0);
         assertEquals(0, mp.getSessionQueueOverrunSum());
         mp.clearSessionQueueFillMax();
+
+    }
+
+    /**
+     * Tests if replacing a non-clean session with a clean one correctly updates
+     * the session count metric.
+     *
+     * @throws MqttException if there is a connection problem.
+     */
+    @Test
+    public void givenClosedNonCleanSessionWhenReconnectAsCleanThenSessionCountMetricsDoesntMissIt() throws MqttException {
+        // Setup, check if the mock metrics provider is used and is clean.
+        MetricsProviderMock mp = validateMetricsProvider();
+        assertEquals(0, mp.getSessionCount());
+
+        // Connect two clients, one with clean session, one with non-clean session.
+        final MqttConnectOptions listenerOptions = new MqttConnectOptions();
+        listenerOptions.setCleanSession(false);
+        clientListener.connect(listenerOptions);
+        clientPublisher.connect();
+
+        // There should now be two open sessions.
+        Awaitility.await("Wait for the metrics to update.")
+            .pollInterval(Duration.ofMillis(10))
+            .atMost(Duration.ofMillis(100))
+            .until(() -> mp.getSessionCount(), Is.is(2));
+
+        // Disconnect the clients, this should leave the non-clean session open.
+        clientListener.disconnect();
+        clientPublisher.disconnect();
+
+        // There should now be one open session, the non-clean listener session.
+        Awaitility.await("Wait for the metrics to update.")
+            .pollInterval(Duration.ofMillis(10))
+            .atMost(Duration.ofMillis(100))
+            .until(() -> mp.getSessionCount(), Is.is(1));
+
+        // Re-open both sessions, but now both set to clean.
+        listenerOptions.setCleanSession(true);
+        clientListener.connect(listenerOptions);
+        clientPublisher.connect();
+
+        // There should now again be two open sessions, old non-clean session should be replaced with a clean one.
+        Awaitility.await("Wait for the metrics to update.")
+            .pollInterval(Duration.ofMillis(10))
+            .atMost(Duration.ofMillis(100))
+            .until(() -> mp.getSessionCount(), Is.is(2));
+
+        // disconnect both sessions, this should leave 0 sessions open.
+        clientListener.disconnect();
+        clientPublisher.disconnect();
+
+        // There should now be no open sessions.
+        Awaitility.await("Wait for the metrics to update.")
+            .pollInterval(Duration.ofMillis(10))
+            .atMost(Duration.ofMillis(100))
+            .until(() -> mp.getSessionCount(), Is.is(0));
 
     }
 
